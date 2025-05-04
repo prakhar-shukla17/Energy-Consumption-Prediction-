b343430c743757dbb992730fc49fdd8e1d16a277
prakhar-shukla17 <prakhars210.26@gmail.com>
2025-05-04 19:27:13 +0530

    data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,9 @@
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -19,6 +23,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-
-        
